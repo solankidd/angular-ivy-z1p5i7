f46f591aac114518d0e6c4487dd2305baa44f2f6
solankidd <dhaval.d.solanki@gmail.com>
2022-03-13 22:13:14 +0530

    4 files modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,32 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
   // loginForm: FormGroup;
+  count = 1;
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngDoCheck(){
+    console.log('do check');
+  }
+
   login(form){
 
     console.log(form['name'].markAsDirty()) 
+  }
+
+  getCount(){
+    console.log('count',++this.count)
   }
 
 }
